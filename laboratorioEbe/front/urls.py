3aea0b7520f56e48e53b243d5a0d0bd405d4aad0
LeydiMontufar <leydimontufar16@gmail.com>
2024-04-21 19:52:29 -0500

    diseño

--- laboratorioEbe/front/urls.py
+++ laboratorioEbe/front/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 urlpatterns = [
     
-    path("", views.index, name="menu"),
+    path("", views.login, name="login"),
+    path("menu", views.menu, name="menu"),
     path("cuadro", views.cuadro, name="cuadro"),
+    path("registro", views.registro, name="registro"),
+    path("menu", views.menu, name="menu"),
+    path("gPerfil", views. gPerfil, name="gPerfil"),
 
 ]
--- laboratorioEbe/front/views.py
+++ laboratorioEbe/front/views.py
@@ -5,11 +5,21 @@ from django.template import loader
 
 # Create your views here.
 #imprime un txto html
-def index(request):
-    template = loader.get_template("front/menu.html")
+def login(request):
+    template = loader.get_template("front/login.html")
     context = {}
     return HttpResponse(template.render(context,request))
+def menu(request):
+    context = {}
+    return render(request, 'front/menu.html', context)
+
 def cuadro(request):
     context = {}
     return render(request, 'front/cuadro_formulario.html', context)
+def registro(request):
+    context = {}
+    return render(request, 'front/registro.html', context)
+def gPerfil(request):
+    context = {}
+    return render(request, 'front/GPerfil.html', context)
 
